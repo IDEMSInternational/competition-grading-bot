61d1ab935e6b584c9ecdd751373db41285bf614f
Georg Os <geoo89@gmail.com>
2023-01-31 10:17:23 +0100

    Create sdaps_tools.py

---
+++ sdaps_tools.py
@@ -0,0 +1,13 @@
+
+
+def setup_sdaps_project(tex_file, project_id):
+    # TODO: Fill in code that sets up an SDAPS project for the given tex
+    # file and with the given project id (=sdaps folder name).
+    pass
+
+def parse_image(image, project_id):
+    # Invokes SDAPS to process the image into a text format (e.g. csv).
+    # Reads the output and prints it to the screen.
+    
+    # TODO: get the actual output and print it (student answers and other info).
+    print("Student: Theo\nQ1: A\nA2: B\nQ3: D\nQ4: A")
